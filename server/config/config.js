bb55a000c58aca4ad10f064d9361f736fc3e9c64
jcamacaro <jesus.alejandr@gmail.com>
2018-12-17 12:02:22 -0430

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if ( process.env.NODE_ENV === 'dev') {
 	urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-	urlDB = 'mongodb://cafe-user:123456p@ds135724.mlab.com:35724/cafe'	;
+	urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;

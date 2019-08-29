51a80c957592f211c5944f57b659cc05103fffd5
jcamacaro <jesus.alejandr@gmail.com>
2019-08-29 13:52:39 -0430

    agregando comentario en el modelo de categoria

--- server/models/categoria.js
+++ server/models/categoria.js
@@ -12,5 +12,8 @@ let categoriaSchema = new Schema({
 		ref: 'Usuario'
 	}
 });
-
+// ==============
+// Primer parametro es la identidad, nombre como tal que se guardara en la db es como si fuera la tabla
+// mongoose lo que hace es que en la base de datos 'Project' lo convertira en miniscula y lo pluralisa 'projects
+// =====================
 module.exports = mongoose.model('Categoria', categoriaSchema);
